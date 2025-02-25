c341a84d9bf855ad4bc471c1aa87e406c46eda19
Paul Pichon F <paul10_barca@hotmail.com>
2025-02-24 20:54:08 -0600

    feat: creacion de proyecto Tsk Frontend

---
+++ src/app/components/Tasks.tsx
@@ -0,0 +1,11 @@
+
+export default function Tasks() {
+    return (
+        <div className="p-4 border rounded-lg shadow-sm bg-gray-50">
+            <h2 className=" text-black text-lg font-semibold">Tarea 1</h2>
+            <p className="text-gray-600">Descripción de la tarea 1.</p>
+            <p className="text-sm text-blue-600">Estado: Pendiente</p>
+            <p className="text-sm text-red-600">Prioridad: Alta</p>
+        </div>
+    );
+}
