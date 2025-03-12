93419490058720869097868b3d345012e60f386f
Paul Pichon F <paul10_barca@hotmail.com>
2025-03-11 21:32:26 -0600

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import Tasks from "./components/Tasks";
+
+import GetTasks from "./components/Tasks";
+// Crear tarea
+import CrearTarea from "@/lib/CrearTarea";
 
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-md">
         <h1 className="text-black text-2xl font-bold mb-4">Lista de Tareas</h1>
         {/* boton para crear una nueva tarea */}
-        <button className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Crear Tarea</button>
+        <CrearTarea />
+        
         <div className="space-y-4">
             
             {/* Tareas */}
-            <Tasks />
+            <GetTasks />
         </div>
     </div>
   );
