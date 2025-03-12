db880e42a73d2bb7f505e1f74a255542534835d2
Paul Pichon F <paul10_barca@hotmail.com>
2025-03-11 21:10:47 -0600

    feat: creacion de componente Task para mostrar las tareas

--- src/app/components/Tasks.tsx
+++ src/app/components/Tasks.tsx
@@ -1,11 +1,43 @@
+// Use server
+'use server';
+// LLamda a API
+import { fetchTasks } from "@/lib/api";
 
-export default function Tasks() {
+export default async function GetTasks() {
+    // Tareas
+    const tasks = await fetchTasks();
+    // Prioridades
+    // ✅ Record<number, string> indica que prioridades es un objeto donde las claves son números y los valores son cadenas.
+    // ✅ Esto elimina el error de TypeScript sin afectar el comportamiento del código.
+    const prioridades: Record<number, string> = {
+        3: 'Baja',
+        2: 'Media',
+        1: 'Alta',
+        0: 'Urgente', //Ajustar el defecto por valor si es necesario
+    };
     return (
-        <div className="p-4 border rounded-lg shadow-sm bg-gray-50">
-            <h2 className=" text-black text-lg font-semibold">Tarea 1</h2>
-            <p className="text-gray-600">Descripción de la tarea 1.</p>
-            <p className="text-sm text-blue-600">Estado: Pendiente</p>
-            <p className="text-sm text-red-600">Prioridad: Alta</p>
-        </div>
+        <ul className="p-4 border rounded-lg shadow-sm bg-gray-50">
+            {tasks.length > 0 ? (
+                tasks.map((task) => 
+                    <li key={task._id}  className="flex justify-between items-center p-4 mt-4 border rounded-lg shadow-sm bg-gray-50">
+                        <h2 className=" text-black text-lg font-semibold">{task.nombre}</h2>
+                        <p className="text-gray-600">{task.descripcion}</p>
+                        <p className="text-sm text-blue-600">Estado: {task.status}</p>
+                        <p className="text-sm text-red-600">
+                            Prioridad: { prioridades[ task.prioridad ] || 'DESCONOCIDA' }
+                        </p>
+
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-right">
+                            Editar
+                        </button>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-right">
+                            Eliminar
+                        </button>
+                    </li>  
+                )
+            ) : (
+                <p>No hay tareas disponibles.</p>
+            )}
+        </ul>
     );
 }
