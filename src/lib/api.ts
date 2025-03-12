51ec4aa0d7a0b33267a221ca316ef8662409bf9e
Paul Pichon F <paul10_barca@hotmail.com>
2025-03-11 21:09:36 -0600

    feat: creacion de Fetch API para la llamada de la API

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,16 +3,18 @@
 import { ApiResponse, Tarea } from "@/styles/interfaces";
 export async function fetchTasks(): Promise<Tarea[]> {
     try {
+        // peticion a la API
         const res = await fetch('http://localhost:5000/api/tasks', {
             cache: "no-store"
         });
-
+        // si la respuesta no es correcta
         if (!res.ok) {
+            // mostramos uns alerta de error
             throw new Error(`Error al obtener tareas: ${res.statusText}`);
         }
-
+        // convertimos la respuesta a JSON en caso de que la respuesta sea exitosa
         const data: ApiResponse = await res.json(); // Convertimos el JSON a nuestra interfaz
-        return data.tasks; // Extraemos solo el array de tareas
+        return data.tasks; // Extraemos y retornamos solo el array de tareas
     } catch (error) {
         console.error("Error:", error);
         return [];
